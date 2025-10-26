f35cab6a888709ed59bcc4813814ed6c3c9f59c5
Leonardo Saito <leosaito632@gmail.com>
2025-10-25 23:44:34 -0300

    remove prints + delete all user data (not the account)

--- backend/app/repository.py
+++ backend/app/repository.py
@@ -1,6 +1,7 @@
 from . import models, schemas
 from sqlalchemy.orm import Session
 from sqlalchemy import update
+from sqlalchemy.sql import or_
 
 
 def create_user(db: Session, user_data: models.Usuario) -> models.Usuario:
@@ -42,3 +43,39 @@ def update_user(
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def get_dado_ids_by_user(db: Session, user_id: int) -> list[int]:
+    """Busca todos os IDs de Dados que pertencem a um usuário."""
+    dado_ids = db.query(models.Dado.id).filter(models.Dado.usuario_id == user_id).all()
+    # isso retorna uma lista de tuplas (por algum motivo)
+    return [d[0] for d in dado_ids]  # transforma a lista de tuplas em uma lista normal
+
+
+def delete_logs_by_user_and_dados(db: Session, user_id: int, dado_ids: list[int]):
+    """Deleta todos os logs associados a um usuário e aos seus dados."""
+    query = db.query(models.Log).filter(
+        or_(models.Log.usuario_id == user_id, models.Log.id_dado.in_(dado_ids))
+    )
+    query.delete(synchronize_session=False)
+
+
+def delete_eventos_by_user(db: Session, user_id: int):
+    """Deleta todos os eventos associados a um usuário."""
+    db.query(models.Evento).filter(models.Evento.usuario_id == user_id).delete(
+        synchronize_session=False
+    )
+
+
+def delete_compartilhamentos_by_user(db: Session, user_id: int):
+    """Deleta todos os compartilhamentos criados por um usuário."""
+    db.query(models.Compartilhamento).filter(
+        models.Compartilhamento.owner_usuario_id == user_id
+    ).delete(synchronize_session=False)
+
+
+def delete_dados_by_user(db: Session, user_id: int):
+    """Deleta todos os Dado' (senhas e arquivos) de um usuário."""
+    db.query(models.Dado).filter(models.Dado.usuario_id == user_id).delete(
+        synchronize_session=False
+    )
